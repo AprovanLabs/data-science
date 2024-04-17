c3849468011568ee53b3db10c15d59817cb9af83
Jacob Sampson <jacob.samps@gmail.com>
2024-04-17 09:51:08 -0500

    Move jobbot code

--- src/jobbot/jobbot/scraper/__init__.py
+++
@@ -1,4 +0,0 @@
-from .jobbot_scraper import JobBotScraper
-from .jobbot_spider import JobBotSpider
-
-__all__ = ["JobBotScraper", "JobBotSpider"]
--- src/jobbot/jobbot/spiders/__init__.py
+++
@@ -1,3 +0,0 @@
-from .minnesota_works_spider import MinnesotaWorksSpider
-
-__all__ = ["MinnesotaWorksSpider"]
